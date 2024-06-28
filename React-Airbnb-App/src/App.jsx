82133b415a7ec5a7f62171f3b2d5e5babbf1bb7e
DevSankale <sankaledev@gmail.com>
2024-06-28 12:37:28 +0300

    Experience Components updated.

--- React-Airbnb-App/src/App.jsx
+++ React-Airbnb-App/src/App.jsx
@@ -15,7 +15,9 @@ function App() {
       reviewCount = {item.stats.reviewCount}
       details={item.title}
       amount={item.price}
-    />
+      openSpots={item.openSpots}
+      location = {item.location}
+      />
     )
   })
   
--- React-Airbnb-App/src/card.jsx
+++ React-Airbnb-App/src/card.jsx
@@ -2,8 +2,16 @@ import React from "react";
 
 export default function Card(props){
 
+    let badgeText
+    if(props.openSpots===0){
+        badgeText = "SOLDOUT"
+    } else if(props.location==="Online"){
+      badgeText = "ONLINE"
+    }
+
   return (
     <div className="card">
+      {badgeText && <div className="card-badge">{badgeText}</div>}
       <img className="card-image"src={props.img}/>
       <div className="ratings">
         <img src="./public/images/star.png"/>
